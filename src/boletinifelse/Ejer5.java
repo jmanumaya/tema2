de83915b8b616b09401fe017abd4dbf6cfb340f1
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-10-06 21:04:15 +0200

    Ejercicio acabado (lo he intentado reducir lo máximo que he sabido y podido).

--- src/boletinifelse/Ejer5.java
+++ src/boletinifelse/Ejer5.java
@@ -3,64 +3,67 @@ package boletinifelse;
 import java.util.Scanner;
 
 public class Ejer5 {
-	
-	public static void main(String[] args) {
-		
-		// Introduzco el escaner a mi programa para su uso.
-		Scanner sc = new Scanner (System.in);
-		// Declaro las variables enteras para albergar los valores que de el usuario.
-		int a;
-		int b;
-		int c;
-		// Le indico por consola al usuario lo que hará el progama y para que me de los datos.
-		System.out.println("Buenas, introduce tres números enteros y te los mostraré ordenadamente de mayor a menor.");
-		System.out.println("Primer Número: ");
-		// Entrada del usuario por teclado para introducir el primer número que se le pide.
-		a = sc.nextInt();
-		System.out.println("Segundo Número: ");
-		// Entrada del usuario por teclado para introducir el segundo número que se le pide.
-		b = sc.nextInt();
-		System.out.println("Tercer Número: ");
-		// Entrada del usuario por teclado para introducir el segundo número que se le pide.
-		c = sc.nextInt();
-		
-		// IF para realizar la ordenación de los números introducidos por el usuario.
-		if (a > b && b > c) {
-			System.out.println(a + " > " + b + " > " + c); // Este if me va a devolver por pantalla que el valor de a es mayor que el de b y el de b es mayor que el de c.
-		} else if (b > a && a > c){
-			System.out.println(b + " > " + a + " > " + c); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (c > b && a > b) {
-			System.out.println(c + " > " + a + " > " + b); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (b > c && c > a) {
-			System.out.println(b + " > " + c + " > " + a); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (c > a && a > b) {
-			System.out.println(c + " > " + a + " > " + b); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a > c && c > b) {
-			System.out.println(a + " > " + c + " > " + b); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (c > b && b > a) {
-			System.out.println(c + " > " + b + " > " + a); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a == b && b == c) {
-			System.out.println(a + " = " + b + " = " + c); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a == b && b > c) {
-			System.out.println(a + " = " + b + " > " + c); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a == c && b < c) {
-			System.out.println(a + " = " + c + " > " + b); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a > b && b == c) {
-			System.out.println(a + " > " + b + " = " + c); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (a == b && c > b) {
-			System.out.println(c + " > " + b + " = " + a); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} else if (b > a && a == c) {
-			System.out.println(b + " > " + a + " = " + c); // Este else if me va a devolver por pantalla que el valor de a es menor que el de b.
-		} 
-		
-		
-		
-		
-		/*else {
-			System.out.println(a + " = " + b); // Este else ha determinado que los número no son iguales.
-		} */
+    public static void main(String[] args) {
+    	
+    	// Creo el escaner para porder usarlo
+        Scanner sc = new Scanner(System.in);
+        
+        // Creo las variables que almacenarán los números que introduzca el usuario
+        int a;
+        int b;
+        int c;
+        // Variables para almacenar los números ordenados
+        int mayor; // Contendrá el número mayor
+        int medio; // Contendrá el número del medio
+        int menor; // Contendrá el número menor
+        
+        // Pedimos los tres números al usuario
+        System.out.print("Introduce el primer número: ");
+        a = sc.nextInt();
+        
+        System.out.print("Introduce el segundo número: ");
+        b = sc.nextInt();
+        
+        System.out.print("Introduce el tercer número: ");
+        c = sc.nextInt();
 
-		// Cierro uso de escaner.
-		sc.close();
-	}
+        // Comparar para determinar el orden.
+        // Primero comprobamos si a es mayor y luego las posiciones de b y c.
+        if (a >= b && a >= c) {
+            mayor = a;
+            if (b >= c) {
+                medio = b;
+                menor = c;
+            } else {
+                medio = c;
+                menor = b;
+            }
+        // Aquí comprobamos si b es el mayor y luego las posiciones de a y c.
+        } else if (b >= a && b >= c) {
+            mayor = b;
+            if (a >= c) {
+                medio = a;
+                menor = c;
+            } else {
+                medio = c;
+                menor = a;
+            }
+        // Si llegasemos hasta aquí sabriamos que c es el mayor y luego se haría las comprobaciones de las posiciones de a y b.
+        } else {
+            mayor = c;
+            if (a >= b) {
+                medio = a;
+                menor = b;
+            } else {
+                medio = b;
+                menor = a;
+            }
+        }
+
+        // Mostrar los números ordenados de mayor a menor después de determinar el orden.
+        System.out.println("Los números ordenados de mayor a menor son: " + mayor + ", " + medio + ", " + menor);
+        
+        // Cerramos uso de escaner.
+        sc.close();
+    }
 }
