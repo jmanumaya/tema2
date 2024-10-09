90256b72db1d6b66bdb99e7715c2af6cc35c0926
jm.maya <jm.maya@106-20.info2>
2024-10-09 09:00:38 +0200

    Ejercicio acabado.

---
+++ src/boletinswitch/Ejer1.java
@@ -0,0 +1,43 @@
+package boletinswitch;
+
+import java.util.Scanner;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		
+		// Creo el escaner para poder usarlo en el programa.
+		Scanner sc = new Scanner (System.in);
+				
+		// Creo la variable entera nota para albergar el valor que de el usuario.
+		int nota;
+				
+		// Le indico al usuario que me introduzca su nota.
+		System.out.print("Dime tu nota: ");
+		// Recojo el dato por parte del usuario.
+		nota = sc.nextInt();
+		
+		// Creo el switch para imprimir en pantalla un caso segun lo que el usuario introduzca.
+		switch (nota) {
+			case 0, 1, 2, 3, 4 -> {
+				System.out.println("INSUFICIENTE"); // Si la nota es o 0, 1, 2, 3, 4, 5 imprime INSUFICIENTE.
+			}
+			case 5 -> {
+				System.out.println("SUFICIENTE"); // Si la nota es o 0, 1, 2, 3, 4, 5 imprime SUFICIENTE.
+			}
+			case 6 -> {
+				System.out.println("BIEN"); // Si la nota es o 0, 1, 2, 3, 4, 5 imprime BIEN.
+			}
+			case 7, 8 -> {
+				System.out.println("NOTABLE"); // Si la nota es o 0, 1, 2, 3, 4, 5 imprime NOTABLE.
+			}	
+			case 9, 10 -> {
+				System.out.println("SOBRESALIENTE"); // Si la nota es o 0, 1, 2, 3, 4, 5 imprime SOBRESALIENTE.
+			}	
+				
+		}
+		//Cierro uso de escaner.
+		sc.close();
+	}
+
+}
