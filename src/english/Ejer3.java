852093773272a3807d8887742da0121697f6b9ec
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-10-21 18:53:28 +0200

    Finish Exercise.

--- src/english/Ejer3.java
+++ src/english/Ejer3.java
@@ -4,44 +4,51 @@ import java.util.Scanner;
 
 public class Ejer3 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		double measure;
+        double measure; // Variable to store the current measure input by the user
 
-		double measureHight = 0;
-		double measureLowest = 0;
+        double measureHight = 0;  // Variable to store the highest measure
+        double measureLowest = 0; // Variable to store the lowest measure
 
-		Scanner sc = new Scanner(System.in);
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println("Put the measure");
-		System.out.print("Measure: ");
+        // Prompt the user to input the first measure
+        System.out.println("Put the measure");
+        System.out.print("Measure: ");
+        
+        // Read the first measure input
+        measure = sc.nextDouble();
+        
+        // Initialize the lowest measure with the first input
+        measureLowest = measure;
 
-		measure = sc.nextDouble();
-		
-		measureLowest = measure;
+        // Continue the loop as long as the measure is greater than 0
+        while (measure > 0) {
 
-		while (measure > 0) {
+            // If the current measure is greater than the highest recorded measure, update it
+            if (measure > measureHight) {
+                measureHight = measure;
+            }
 
-			if (measure > measureHight) {
+            // If the current measure is smaller than the lowest recorded measure, update it
+            if (measureLowest > measure) {
+                measureLowest = measure;
+            }
 
-				measureHight = measure;
+            // Ask the user for another measure
+            System.out.print("Measure: ");
+            measure = sc.nextDouble(); // Read the next measure
 
-			}
-			
-			if (measureLowest > measure) {
-				measureLowest = measure;
-			}
+        }
 
-			System.out.print("Measure: ");
-			measure = sc.nextDouble();
+        // After the loop, display the highest and lowest measures recorded
+        System.out.println(
+                "The highest measure is: " + measureHight + " cm.\nThe lowest measure is: " + measureLowest + " cm.");
 
-		}
-
-		System.out.println(
-				"The hight measure is: " + measureHight + " cm.\nThe lowest measure is: " + measureLowest + " cm.");
-
-		sc.close();
-
-	}
-
+        // Close the scanner to avoid resource leaks
+        sc.close();
+    }
 }
+
--- src/english/Ejer4.java
+++ src/english/Ejer4.java
@@ -1,40 +1,50 @@
 package english;
-import java.util.*;
 
+import java.util.*;
 
 public class Ejer4 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner (System.in);
-		
+
+		// Create a Scanner object to read input from the user
+		Scanner sc = new Scanner(System.in);
+
+		// Create a Random object to generate a random number
 		Random rd = new Random();
-		
-		int number;
-		
+
+		int number; // Variable to store the user's guess
+
+		// Generate a random secret number between 1 and 100
 		int secretNumber = rd.nextInt(100) + 1;
-		
-		
+
+		// Prompt the user to start guessing the secret number
 		System.out.println("You should guess the secret number");
 		System.out.println("What is the secret number?");
+
+		// Read the first guess from the user
 		number = sc.nextInt();
-		
+
+		// Continue looping until the user guesses the correct number
 		while (number != secretNumber) {
-			
+
+			// If the guess is lower than the secret number
 			if (number < secretNumber) {
-				
 				System.out.println("The secret number is greater than your number");
-			} else if (number > secretNumber) {
-				
+			}
+			// If the guess is higher than the secret number
+			else if (number > secretNumber) {
 				System.out.println("The secret number is less than your number");
 			}
-			
+
+			// Prompt the user to guess again
 			System.out.println("What is the secret number?");
-			number = sc.nextInt();
+			number = sc.nextInt(); // Read the new guess
 		}
-		
-		System.out.println("Congrats! you guess the secret number!!!");
-		
+
+		// If the user guesses the correct number, print the success message
+		System.out.println("Congrats! You guessed the secret number!!!");
+
+		// Close the scanner to avoid resource leaks
 		sc.close();
 	}
 }
