c543309527bb188619e3ce924c3f5efc5b4cdbb0
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-10-29 00:35:35 +0100

    Acabados

---
+++ src/boletinbucles/Ejer4.java
@@ -0,0 +1,53 @@
+package boletinbucles;
+
+import java.util.Scanner;
+
+public class Ejer4 {
+
+    public static void main(String[] args) {
+        
+        // Creao el escáner para poder usarlo.
+        Scanner sc = new Scanner(System.in);
+        
+        // Variables para los dos números indicados por el usuario.
+        int a, b;
+        
+        // Esta variable va a contener el menor de los dos valores introducidos.
+        int menor;
+        
+        // Inicializo el mcd a 1.
+        int mcd = 1;
+        
+        // Variable que contendrá el valor del mcm final.
+        int mcm;
+        
+        // Solicito al usuario los dos números y le explico que hará el programa.
+        System.out.println("Ingresa dos números y te diré su mínimo común múltiplo.");
+        System.out.print("Ingrese el primer número: ");
+        a = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        b = sc.nextInt();
+
+        // Encontrar el MCD de los dos números
+        menor = Math.min(a, b);
+
+        // Realizamos el bucle desde el número menor hasta 1 para encontrar el MCD (igual que en el 3).
+        for (int i = menor; i > 0; i--) {
+        	
+            if (a % i == 0 && b % i == 0) {
+                mcd = i;
+                break;
+            }
+        }
+
+        // Calculo para obtener el MCM usando la fórmula: MCM(a y b) = |a * b| / MCD(a y b)
+        mcm = (a * b) / mcd;
+
+        // Muestro el resultado al usuario.
+        System.out.println("El MCM de " + a + " y " + b + " es: " + mcm);
+        
+        // Cerrar el escáner.
+        sc.close();
+    }
+}
+
--- src/boletinbucles/Ejer6.java
+++ src/boletinbucles/Ejer6.java
@@ -6,32 +6,36 @@ public class Ejer6 {
 
 	public static void main(String[] args) {
 		
-		
+		// Esta variable contendrá el valor indicado por el usuario.
 		int num;
 		
-		int cont = 1;
-		
+		// Creo el escaner para poder usarlo
 		Scanner sc = new Scanner (System.in);
 		
+		// Indico al usuario que tiene que hacer y que hará el programa.
+		System.out.println("Dame la base de un triangulo. (la altura será la misma)");
 		
-		System.out.println("Dame la base de un triangulo.");
+		// Pido la base del triangulo
 		System.out.println("Base: ");
 		num = sc.nextInt();
 		
 		
-		for (int i = num; i > 0; --i) {
-			
-			for (int x = 1; x <= cont; x++) {
-			
-				System.out.print(" * ");
+		// Dibuja el triángulo
+        for (int i = 1; i <= num; i++) {
+        	
+            // Imprime los espacios antes de los asteriscos
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+            // Imprime los asteriscos con espacios intermedios
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            // Salta a la siguiente línea después de imprimir los asteriscos
+            System.out.println();
+        }
 
-			}
-			
-			System.out.println("");
-			++cont;
-			
-		}
-
+        // Cierro uso de escaner.
 		sc.close();
 	}
 
